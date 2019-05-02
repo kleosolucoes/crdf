d34c09ebbceee8ab5aa62169a561c86d6433a283
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2019-05-02 17:04:15 -0300

    logger comentado

--- src/index.js
+++ src/index.js
@@ -19,14 +19,14 @@ var config = {
 	messagingSenderId: "459879150044"
 };
 firebase.initializeApp(config);
-const logger = store => next => action => {
-	console.group(action.type)
-	console.info('despachando', action)
-	let resultado = next(action)
-	console.log('proximo', store.getState())
-	console.groupEnd(action.type)
-	return resultado
-}
+//const logger = store => next => action => {
+//	console.group(action.type)
+//	console.info('despachando', action)
+//	let resultado = next(action)
+//	console.log('proximo', store.getState())
+//	console.groupEnd(action.type)
+//	return resultado
+//}
 //const store = createStore(rootReducer, applyMiddleware(logger, thunk))
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
